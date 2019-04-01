34debc4a4bf9a6071a9a06535e5bd8cedef708d2
r.quidet <r.quidet@sportradar.com>
2019-04-01 14:49:40 +0200

    Cancels edit.

--- content.php
+++ content.php
@@ -5,39 +5,20 @@ require_once("vendor/autoload.php");
 use FileManager\Display;
 use FileManager\FileHandler;
 
-function saveContent() {
-    if (isset($_POST["content"])) {
-        $display = new Display(new FileHandler());
-        $display->saveContent($_POST["name"], $_POST["content"]);
-    }
-}
+$display = new Display(new FileHandler());
 
-function displayContent() {
-    $display = new Display(new FileHandler());
-    if (isset($_GET["name"])) {
-        echo $display->displayFileContent($_GET["name"]);
-    } elseif (isset($_POST["name"])) {
-        echo $display->displayFileContent($_POST["name"]);
-    }
-}
-
-function getName() {
-    $_GET['name'] = $_POST['name'];
-}
-
-?>
+ ?>
 
 <html lang="en">
     <body>
-        <form action="content.php" method="post">
+        <form action="contentsaved.php" method="post">
             <textarea name="content" rows="30" cols="70">
-                <?php displayContent(); ?>
+                <?php echo $display->displayFileContent($_GET["name"]); ?>
             </textarea>
             <button type="button" onclick="window.location.href='index.php'">Back To Start</button>
-            <input name="name" type="hidden" value="<?=$_POST["name"]?>">
-            <button type="submit" onclick="<?php saveContent();?>">Save</button>
-            <button type="button" onclick="window.location.reload();">Refresh</button>
-            <button type="button">Cancel</button>
+            <input name="name" type="hidden" value="<?=$_GET["name"]?>">
+            <button type="submit">Save</button>
+            <button type="button" onclick="window.location.reload();">Cancel</button>
         </form>
     </body>
 </html>
